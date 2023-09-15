4416dd4c9be7e0f2a971a87f7bb08a73234ebfc8
Rafael Pivato <rpivato@gmail.com>
2023-09-14 23:32:14 -0300

    Market with holidays module added

---
+++ src/tradinghours/market.py
@@ -0,0 +1,72 @@
+from tradinghours.base import BaseObject, BooleanField, DateField, StringField
+
+
+class Market(BaseObject):
+    """One known market for TradingHours"""
+
+    fin_ud = StringField()
+    """The FinID for the market."""
+
+    country_code = StringField()
+    """Two-letter country code."""
+
+    exchange = StringField()
+    """The exchange name of the market."""
+
+    market = StringField()
+    """The name of the market."""
+
+    products = StringField()
+    """Description of the products or securities group."""
+
+    mic = StringField()
+    """The MIC for the market."""
+
+    mic_extended = StringField()
+    """The extended MIC for the market."""
+
+    acronym = StringField()
+    """The acronym for the market."""
+
+    asset_type = StringField()
+    """Describes the asset type of the market."""
+
+    memo = StringField()
+    """A description or additional details about the trading venue."""
+
+    permanently_closed = DateField()
+    """If a market is permanently closed, this shows the date."""
+
+    timezone = StringField()
+    """Gives the timezone the market utilizes."""
+
+    weekend_definition = StringField()
+    """Indicates the days of the week when the market regularly closed."""
+
+
+class MarketHoliday(BaseObject):
+    """Holidays for a Market"""
+
+    fin_id = StringField()
+    """The FinID for the market."""
+
+    date = DateField()
+    """Shows the date of the holiday for the market."""
+
+    name = StringField()
+    """Describes the name of the holiday."""
+
+    schedule = StringField()
+    """Describes if the market closes for the holiday."""
+
+    is_open = BooleanField()
+    """Displays in true/false if the market is open for the holiday."""
+
+    has_settlement = BooleanField()
+    """Displays in true/false if the market has settlement for the holiday."""
+
+    observed = BooleanField()
+    """Displays in true/false if the holiday is observed."""
+
+    memo = StringField()
+    """A description or additional details about the holiday."""
