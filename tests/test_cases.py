bb0cf3e01041551f735c1df71bb5031bf2069923
andreigoulart <goulartandreidh@gmail.com>
2023-11-21 11:32:10 -0300

    Start testing edge cases

---
+++ tests/test_cases.py
@@ -0,0 +1,39 @@
+import unittest
+
+from tradinghours.market import Market
+
+
+class EdgeCase(unittest.TestCase):
+    def setUp(self):
+        self.market = None
+
+    def assertRangeSchedule(self, fin_id, start, end, expected):
+        market = Market.get(fin_id)
+        schedules = market.generate_schedules(start, end)
+        phases = map(lambda s: s.phase_type, schedules)
+        result = list(phases)
+        self.assertEqual(result, expected)
+
+    def assertDateSchedule(self, fin_id, date, expected):
+        self.assertRangeSchedule(fin_id, date, date, expected)
+
+
+class TestCase001(EdgeCase):
+    """
+
+    Check there are correct schedules on a regularly open day, no holiday
+
+    """
+
+    def test_case(self):
+        fin_id = "US.NYSE"
+        date = "2023-11-15"
+        expected = [
+            "Pre-Trading Session",
+            "Pre-Open",
+            "Call Auction",
+            "Primary Trading Session",
+            "Pre-Close",
+            "Post-Trading Session",
+        ]
+        self.assertDateSchedule(fin_id, date, expected)
