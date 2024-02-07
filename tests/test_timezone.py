a762f5ff0208784e6134468a32b226806973ee20
Stryder-Git <marcel.pieper@outlook.com>
2024-02-07 14:20:17 -0600

    trying to figure out weird DST behavior

--- tests/test_timezone.py
+++ tests/test_timezone.py
@@ -1,42 +1,63 @@
-# import pytest
-# import pytz
-# from datetime import datetime, timedelta
+import pytest
+import pytz
+from zoneinfo import ZoneInfo
+from datetime import datetime, timedelta
+
+# Assume we have a function `convert_to_timezone` that converts given naive datetime to the specified timezone
+# from yourmodule import convert_to_timezone
+
+# def make_timezone_obj(tz):
+#     return pytz.timezone(tz)
 #
-# # Assume we have a function `convert_to_timezone` that converts given naive datetime to the specified timezone
-# # from yourmodule import convert_to_timezone
+# def convert_to_timezone(naive_dt, timezone_str):
+#     tz_obj = make_timezone_obj(timezone_str)
+#     return tz_obj.localize(naive_dt)
+#
+
+def make_timezone_obj(tz):
+    return ZoneInfo(tz)
+
+def convert_to_timezone(naive_dt, timezone_str):
+    tz_obj = make_timezone_obj(timezone_str)
+    return naive_dt.replace(tzinfo=tz_obj)
+
+
 #
 # @pytest.mark.parametrize("naive_dt, timezone_str, expected_dt", [
 #     # Test DST start transition (US/Eastern, 2nd Sunday in March)
-#     (datetime(2022, 3, 13, 2), "US/Eastern", datetime(2022, 3, 13, 3, tzinfo=pytz.timezone("US/Eastern"))),
+#     (datetime(2022, 3, 13, 2), "US/Eastern", convert_to_timezone(datetime(2022, 3, 13, 3), "US/Eastern")),
 #     # Test DST end transition (US/Eastern, 1st Sunday in November)
-#     (datetime(2022, 11, 6, 1), "US/Eastern", datetime(2022, 11, 6, 1, tzinfo=pytz.timezone("US/Eastern"))),
+#     (datetime(2022, 11, 6, 1), "US/Eastern", convert_to_timezone(datetime(2022, 11, 6, 1), "US/Eastern")),
 # ])
 # def test_dst_transitions(naive_dt, timezone_str, expected_dt):
-#     assert convert_to_timezone(naive_dt, timezone_str) == expected_dt
-#
-# @pytest.mark.parametrize("dt, days, expected_dt", [
-#     # Adding days over a leap day (non-leap year)
-#     (datetime(2021, 2, 28, tzinfo=pytz.utc), 1, datetime(2021, 3, 1, tzinfo=pytz.utc)),
-#     # Adding days over a leap day (leap year)
-#     (datetime(2024, 2, 28, tzinfo=pytz.utc), 1, datetime(2024, 2, 29, tzinfo=pytz.utc)),
-# ])
-# def test_leap_years(dt, days, expected_dt):
-#     assert dt + timedelta(days=days) == expected_dt
+#     converted = convert_to_timezone(naive_dt, timezone_str)
+#     print(converted, expected_dt, converted == expected_dt)
+#     assert converted == expected_dt, str(converted) + "==" + str(expected_dt)
+
+@pytest.mark.parametrize("dt, days, expected_dt", [
+    # Adding days over a leap day (non-leap year)
+    (convert_to_timezone(datetime(2021, 2, 28), "UTC"), 1, convert_to_timezone(datetime(2021, 3, 1), "UTC")),
+    # Adding days over a leap day (leap year)
+    (convert_to_timezone(datetime(2024, 2, 28), "UTC"), 1, convert_to_timezone(datetime(2024, 2, 29), "UTC")),
+])
+def test_leap_years(dt, days, expected_dt):
+    assert dt + timedelta(days=days) == expected_dt
+
 #
 # @pytest.mark.parametrize("dt, hours, timezone_str, expected_dt", [
 #     # Subtracting hours across DST start (spring forward)
-#     (datetime(2022, 3, 13, 10, tzinfo=pytz.timezone("US/Eastern")), -8, "US/Eastern", datetime(2022, 3, 13, 1, tzinfo=pytz.timezone("US/Eastern"))),
+#     (convert_to_timezone(datetime(2022, 3, 13, 10), "US/Eastern"), -8, "US/Eastern", convert_to_timezone(datetime(2022, 3, 13, 1), "US/Eastern")),
 #     # Adding hours across DST end (fall back)
-#     (datetime(2022, 11, 6, 1, tzinfo=pytz.timezone("US/Eastern")), 2, "US/Eastern", datetime(2022, 11, 6, 1, tzinfo=pytz.timezone("US/Eastern"))),
+#     (convert_to_timezone(datetime(2022, 11, 6, 1), "US/Eastern"), 2, "US/Eastern", convert_to_timezone(datetime(2022, 11, 6, 1), "US/Eastern")),
 # ])
 # def test_datetime_arithmetic_across_dst(dt, hours, timezone_str, expected_dt):
 #     assert dt + timedelta(hours=hours) == expected_dt
-#
-# @pytest.mark.parametrize("dt, new_timezone_str, expected_dt", [
-#     # Convert time from UTC to Asia/Tokyo
-#     (datetime(2022, 1, 1, 0, tzinfo=pytz.utc), "Asia/Tokyo", datetime(2022, 1, 1, 9, tzinfo=pytz.timezone("Asia/Tokyo"))),
-#     # Convert time from Asia/Tokyo to Europe/London
-#     (datetime(2022, 1, 1, 0, tzinfo=pytz.timezone("Asia/Tokyo")), "Europe/London", datetime(2021, 12, 31, 15, tzinfo=pytz.timezone("Europe/London"))),
-# ])
-# def test_timezone_conversion(dt, new_timezone_str, expected_dt):
-#     assert dt.astimezone(pytz.timezone(new_timezone_str)) == expected_dt
+
+@pytest.mark.parametrize("dt, new_timezone_str, expected_dt", [
+    # Convert time from UTC to Asia/Tokyo
+    (convert_to_timezone(datetime(2022, 1, 1, 0), "UTC"), "Asia/Tokyo", convert_to_timezone(datetime(2022, 1, 1, 9), "Asia/Tokyo")),
+    # Convert time from Asia/Tokyo to Europe/London
+    (convert_to_timezone(datetime(2022, 1, 1, 0), "Asia/Tokyo"), "Europe/London", convert_to_timezone(datetime(2021, 12, 31, 15), "Europe/London")),
+])
+def test_timezone_conversion(dt, new_timezone_str, expected_dt):
+    assert dt.astimezone(make_timezone_obj(new_timezone_str)) == expected_dt
