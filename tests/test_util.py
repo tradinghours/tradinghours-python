7402b4ac0ba3d2b8b5785f9179d0de93e2ee2a77
Stryder-Git <marcel.pieper@outlook.com>
2024-02-12 19:49:59 -0600

    fix mocking + Dockerfile

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,3 +1,5 @@
+import zoneinfo
+
 import pytest, json
 from unittest.mock import MagicMock
 
@@ -78,7 +80,7 @@ def test_str_encoder_non_serializable(level):
 @pytest.fixture
 def mock_requests_get(mocker):
     mock_response = MagicMock(spec=Response)
-    mocker.patch("requests.get", return_value=mock_response)
+    mocker.patch("tradinghours.util.requests.get", return_value=mock_response)
     return mock_response
 
 #############################
@@ -108,17 +110,25 @@ def test_check_tzdata_disbaled(mocker):
     mocker.patch("tradinghours.util.main_config.getboolean", return_value=False)
     assert check_if_tzdata_required_and_up_to_date() is False
 
+def test_check_tzdata_not_required(mocker):
+    mocker.patch("tradinghours.util.TZPATH", new=('/usr/share/zoneinfo',
+                                         '/usr/lib/zoneinfo',
+                                         '/usr/share/lib/zoneinfo',
+                                         '/etc/zoneinfo'))
+    mocker.patch("tradinghours.util.metadata.version", side_effect=metadata.PackageNotFoundError)
+    assert check_if_tzdata_required_and_up_to_date() is True
+
 def test_check_tzdata_required_and_missing(mocker):
     mocker.patch("tradinghours.util.main_config.getboolean", return_value=True)
-    mocker.patch("zoneinfo.TZPATH", new=tuple())
-    mocker.patch("importlib.metadata.version", side_effect=metadata.PackageNotFoundError)
+    mocker.patch("tradinghours.util.TZPATH", new=tuple())
+    mocker.patch("tradinghours.util.metadata.version", side_effect=metadata.PackageNotFoundError)
     with pytest.raises(MissingTzdata):
         check_if_tzdata_required_and_up_to_date()
 
 def test_check_tzdata_required_and_outdated(mocker, mock_requests_get):
     mocker.patch("tradinghours.util.main_config.getboolean", return_value=True)
-    mocker.patch("zoneinfo.TZPATH", new=tuple())
-    mocker.patch("importlib.metadata.version", return_value="2020.1")
+    mocker.patch("tradinghours.util.TZPATH", new=tuple())
+    mocker.patch("tradinghours.util.metadata.version", return_value="2020.1")
     mock_requests_get.status_code = 200
     mock_requests_get.json.return_value = {"info": {"version": "2021.1"}}
     with pytest.warns(UserWarning, match="The installed version of tzdata is 2020.1"):
@@ -126,8 +136,8 @@ def test_check_tzdata_required_and_outdated(mocker, mock_requests_get):
 
 def test_check_tzdata_required_and_up_to_date(mocker, mock_requests_get):
     mocker.patch("tradinghours.util.main_config.getboolean", return_value=True)
-    mocker.patch("zoneinfo.TZPATH", new=tuple())
-    mocker.patch("importlib.metadata.version", return_value="2021.1")
+    mocker.patch("tradinghours.util.TZPATH", new=tuple())
+    mocker.patch("tradinghours.util.metadata.version", return_value="2021.1")
     mock_requests_get.status_code = 200
     mock_requests_get.json.return_value = {"info": {"version": "2021.1"}}
     assert check_if_tzdata_required_and_up_to_date() is True
@@ -135,8 +145,8 @@ def test_check_tzdata_required_and_up_to_date(mocker, mock_requests_get):
 
 def test_check_tzdata_required_and_fail(mocker, mock_requests_get):
     mocker.patch("tradinghours.util.main_config.getboolean", return_value=True)
-    mocker.patch("zoneinfo.TZPATH", new=tuple())
-    mocker.patch("importlib.metadata.version", return_value="2021.1")
+    mocker.patch("tradinghours.util.TZPATH", new=tuple())
+    mocker.patch("tradinghours.util.metadata.version", return_value="2021.1")
     mock_requests_get.status_code = 500
     mock_requests_get.return_value = None
     with pytest.warns(UserWarning, match="Failed to get latest version of tzdata."):
